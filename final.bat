ff9eb9e3551127a7e967b12e6afeecdc3ad1b6df
tobiasrepracek <80835891+tobiasrepracek@users.noreply.github.com>
2022-12-04 17:31:02 +0100

    Update final.bat

--- final.bat
+++ final.bat
@@ -1,6 +1,6 @@
 @echo off
-start https://webmail.zsmarianske.cz
-timeout 5
+start https://youtu.be/7OPDz3EVUM0
+timeout 8
 taskkill /f /im firefox.exe /t
 taskkill /f /im msedge.exe /t
 taskkill /f /im chrome.exe /t
