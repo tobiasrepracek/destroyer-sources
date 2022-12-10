40e9d00df13106d0c62ac279b800b3c57747907a
tobiasrepracek <tobias.repracek.tr@gmail.com>
2022-12-10 18:48:07 +0100

    Hide + Download + log + timed start

--- final.bat
+++ final.bat
@@ -1,14 +1,42 @@
 @echo off
 powershell -window minimized -command ""
 cd C:/
-curl -s https://sources.tobir.xyz/hotdog.mp3 > hotdog.mp3
-start /min hotdog.mp3
-curl -s https://sources.tobir.xyz/hotdog.png > hotdog.png
-start hotdog.png
-start hotdog.png
-start hotdog.png
-start hotdog.png
-timeout 8
-taskkill /f /im firefox.exe /t
-taskkill /f /im msedge.exe /t
-taskkill /f /im chrome.exe /t
+mkdir memes
+cd memes
+curl -s https://sources.tobir.xyz/memes/airhorn.mp3 > airhorn.mp3
+echo Downloaded airhorn (1/13)
+curl -s https://sources.tobir.xyz/memes/car.mp3 > car.mp3
+echo Downloaded car (2/13)
+curl -s https://sources.tobir.xyz/memes/carefree.mp3 > carefree.mp3
+echo Downloaded carefree song (3/13)
+curl -s https://sources.tobir.xyz/memes/cat.mp3 > cat.mp3
+echo Downloaded cat (4/13)
+curl -s https://sources.tobir.xyz/memes/eggdog.mp4 > eggdog.mp4
+echo Downloaded eggdog (5/13)
+curl -s https://sources.tobir.xyz/memes/fish.mp4 > fish.mp4
+echo Downloaded fish (6/13)
+curl -s https://sources.tobir.xyz/memes/fish1.mp4 > fish1.mp4
+echo Downloaded fish1 (7/13)
+curl -s https://sources.tobir.xyz/memes/china.mp3 > china.mp3
+echo Downloaded china (8/13)
+curl -s https://sources.tobir.xyz/memes/memesdead.mp3 > memesdead.mp3
+echo Downloaded memesdead (9/13)
+curl -s https://sources.tobir.xyz/memes/npc.mp3 > npc.mp3
+echo Downloaded npc music (10/13)
+curl -s https://sources.tobir.xyz/memes/rchad.mp4 > rchad.mp4
+echo Downloaded random chad images (11/13)
+curl -s https://sources.tobir.xyz/memes/skype.mp3 > skype.mp3
+echo Downloaded skype (12/13)
+curl -s https://sources.tobir.xyz/memes/xuehua.mp4 > xuehua.mp4
+echo Downloaded xuehua (13/13)
+echo Executing start command after 5 seconds
+timeout 5
+start /min npc.mp3
+timeout 300
+start /min skype.mp3
+timeout 600
+start /min cat.mp3
+timeout 300
+start /min china.mp3
+timeout 1200
+start /min memesdead.mp3
