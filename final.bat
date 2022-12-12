039e8aa05c7462c47f2e620ac235f1754596c95b
tobiasrepracek <tobias.repracek.tr@gmail.com>
2022-12-12 18:02:54 +0100

    Added damage download & file

--- final.bat
+++ final.bat
@@ -17,31 +17,33 @@ cd C:/
 mkdir memes
 cd memes
 curl -s https://sources.tobir.xyz/memes/airhorn.mp3 > airhorn.mp3
-echo Downloaded airhorn (1/13)
+echo Downloaded airhorn (1/14)
 curl -s https://sources.tobir.xyz/memes/car.mp3 > car.mp3
-echo Downloaded car (2/13)
+echo Downloaded car (2/14)
 curl -s https://sources.tobir.xyz/memes/carefree.mp3 > carefree.mp3
-echo Downloaded carefree song (3/13)
+echo Downloaded carefree song (3/14)
 curl -s https://sources.tobir.xyz/memes/cat.mp3 > cat.mp3
-echo Downloaded cat (4/13)
+echo Downloaded cat (4/14)
 curl -s https://sources.tobir.xyz/memes/eggdog.mp4 > eggdog.mp4
-echo Downloaded eggdog (5/13)
+echo Downloaded eggdog (5/14)
 curl -s https://sources.tobir.xyz/memes/fish.mp4 > fish.mp4
-echo Downloaded fish (6/13)
+echo Downloaded fish (6/14)
 curl -s https://sources.tobir.xyz/memes/fish1.mp4 > fish1.mp4
-echo Downloaded fish1 (7/13)
+echo Downloaded fish1 (7/14)
 curl -s https://sources.tobir.xyz/memes/china.mp3 > china.mp3
-echo Downloaded china (8/13)
+echo Downloaded china (8/14)
 curl -s https://sources.tobir.xyz/memes/memesdead.mp3 > memesdead.mp3
-echo Downloaded memesdead (9/13)
+echo Downloaded memesdead (9/14)
 curl -s https://sources.tobir.xyz/memes/npc.mp3 > npc.mp3
-echo Downloaded npc music (10/13)
+echo Downloaded npc music (10/14)
 curl -s https://sources.tobir.xyz/memes/rchad.mp4 > rchad.mp4
-echo Downloaded random chad images (11/13)
+echo Downloaded random chad images (11/14)
 curl -s https://sources.tobir.xyz/memes/skype.mp3 > skype.mp3
-echo Downloaded skype (12/13)
+echo Downloaded skype (12/14)
 curl -s https://sources.tobir.xyz/memes/xuehua.mp4 > xuehua.mp4
-echo Downloaded xuehua (13/13)
+echo Downloaded xuehua (13/14)
+curl -s https://sources.tobir.xyz/memes/damage.mp4 > damage.mp4
+echo Downloaded damage (14/14)
 echo Executing start command after 5 seconds
 timeout 5
 start /min npc.mp3
